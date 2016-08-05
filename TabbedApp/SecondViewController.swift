c848e729e47cd8c21a7eaf4a0c374b2fd47d38c5
Scott Richards <Scott.Richards@babycenter.com>
2016-08-05 16:25:38 -0700

    Working on scrollers.

--- TabbedApp/SecondViewController.swift
+++ TabbedApp/SecondViewController.swift
@@ -8,24 +8,58 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var contentView: UIView!
     @IBOutlet var scrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("contentViewSize: \(self.contentView.frame.size)")
-        print("self.scrollView.contentSize: \(self.scrollView.contentSize)")
-        self.scrollView.contentSize = self.contentView.frame.size
-        print("After self.scrollView.contentSize: \(self.scrollView.contentSize)")
-
+//        print("contentViewSize: \(self.contentView.frame.size)")
+//        print("self.scrollView.contentSize: \(self.scrollView.contentSize)")
+//        self.scrollView.contentSize = self.contentView.frame.size
+//        print("After self.scrollView.contentSize: \(self.scrollView.contentSize)")
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(SecondViewController.keyboardWillShow(_:)),
+                                                         name: UIKeyboardWillShowNotification,
+                                                         object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self,
+                                                         selector: #selector(SecondViewController.keyboardWillHide(_:)),
+                                                         name: UIKeyboardWillHideNotification,
+                                                         object: nil)
     }
 
+    func adjustInsetForKeyboardShow(show: Bool, notification: NSNotification) {
+        let userInfo = notification.userInfo ?? [:]
+        let keyboardFrame = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        let adjustmentHeight = (CGRectGetHeight(keyboardFrame) + 20) * (show ? 1 : -1)
+        print("show keyboard: \(show)")
+        print("adjustmentHeight: \(adjustmentHeight)")
+        scrollView.contentInset.bottom += adjustmentHeight
+        scrollView.scrollIndicatorInsets.bottom += adjustmentHeight
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        adjustInsetForKeyboardShow(true, notification: notification)
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        adjustInsetForKeyboardShow(false, notification: notification)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    /* This will dismiss the keyboard when the user touches anywhere on the screen */
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
